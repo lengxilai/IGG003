b2a63a9d2940425db46f9c882a7f1d1bd5279bd4
wangchong0114 <wangchong0114@vip.qq.com>
2012-08-01 14:40:10 +0800

    alart

--- IGG003/CL02.m
+++ IGG003/CL02.m
@@ -13,13 +13,18 @@
 static int pauseTime = 5;
 //加时间
 static int addTime = 5;
+- (void) dealloc
+{
+	[super dealloc];
+
+}   
 -(id) init{
     if( (self=[super init])) {
-        CCLabelBMFont *pointsSprit = [CCLabelBMFont labelWithString:@"01:00" fntFile:@"bitmapFont.fnt"];
+        CCLabelBMFont *pointsSprit = [CCLabelBMFont labelWithString:@"01:00" fntFile:@"bitmapFont2.fnt"];
 		pointsSprit.position = ccp(40,450);
         pointsSprit.tag = 100001;
        
-        [self addChild:pointsSprit z:1 tag:3];
+        [self addChild:pointsSprit z:1];
         time = [[NSDate dateWithTimeIntervalSinceNow:(60)] retain];
         [self schedule:@selector(updateTimeDisplay) interval:0.2];
         CCMenuItem  *button = [CCMenuItemImage
@@ -30,7 +35,16 @@ static int addTime = 5;
                                itemFromNormalImage:@"Icon-Small.png" selectedImage:@"Icon-Small.png"
                                target:self selector:@selector(clickIceTool)];
         button2.position =  ccp(80, 380);
-        CCMenu *starMenu = [CCMenu menuWithItems:button,button2, nil];
+        
+        CCMenuItem  *button3 = [CCMenuItemImage
+                                itemFromNormalImage:@"Icon-Small.png" selectedImage:@"Icon-Small.png"
+                                target:self selector:@selector(pauseGame)];
+        button3.position =  ccp(120, 380);
+        CCMenuItem  *button4 = [CCMenuItemImage
+                                itemFromNormalImage:@"Icon-Small.png" selectedImage:@"Icon-Small.png"
+                                target:self selector:@selector(endPause)];
+        button4.position =  ccp(160, 380);
+        CCMenu *starMenu = [CCMenu menuWithItems:button,button2,button3,button4, nil];
         starMenu.position = CGPointZero;
         [self addChild:starMenu];
     }
@@ -39,8 +53,17 @@ static int addTime = 5;
 - (void) updateTimeDisplay{
     
     times = (int)[time timeIntervalSinceNow];
-    CCLabelBMFont *clockLabel = (CCLabelBMFont *)[self getChildByTag:3];
+    CCLabelBMFont *clockLabel = (CCLabelBMFont *)[self getChildByTag:100001];
+
+    if(times ==10){
+        [self removeChildByTag:100001 cleanup:true];
+        clockLabel = [CCLabelBMFont labelWithString:[self stringForObjectValue:[NSNumber numberWithInt: times]] fntFile:@"bitmapFont.fnt"];
+        clockLabel.tag = 100001;
+        clockLabel.position = ccp(40,450);
+        [self addChild:clockLabel];
+    }
     [clockLabel setString:[self stringForObjectValue:[NSNumber numberWithInt: times]]];
+    
     if(times <= 0){
         
         [self unschedule:@selector(updateTimeDisplay)];
@@ -81,20 +104,46 @@ static int addTime = 5;
 }
 //使用冰冻道具
 -(void)clickIceTool{
-    [self unschedule:@selector(updateTimeDisplay)];
-    [self schedule:@selector(pauseSchedule) interval:pauseTime];
-}
-//暂停后调用
--(void)pauseSchedule{
-    times = times + 1;
-    time = [[NSDate dateWithTimeIntervalSinceNow:(times)] retain];
-    [self unschedule:@selector(pauseSchedule)];
-    [self schedule:@selector(updateTimeDisplay) interval:0.2];
+    CCSprite *bg = (CCSprite *)[self getChildByTag:100010];
+    if(!bg){
+        bg = [CCSprite spriteWithFile:@"pop.png"];
+        bg.position = ccp(100,340); //位置
+        bg.tag = 100010;
+    }
+    iceNSDateTime = [[NSDate dateWithTimeIntervalSinceNow:(pauseTime)] retain];
     
+    CCFiniteTimeAction *action0 = [CCFadeOut actionWithDuration:1.1];
+    CCFiniteTimeAction *action1 = [CCFadeIn actionWithDuration:1.1];
+    CCFiniteTimeAction *a3 = [CCSequence actions:action1,action0,action1,action0,nil];
+    [bg runAction:a3];
+    [self addChild:bg]; //将精灵加到layer上 
+    [self unschedule:@selector(updateTimeDisplay)];
+    [self schedule:@selector(pauseScheduleByIce) interval:1];
+    
+}
+//冰冻暂停后调用
+-(void)pauseScheduleByIce{
+    int pauseTimes = (int)[iceNSDateTime timeIntervalSinceNow];
+    
+    if(pauseTimes <= 0){
+        times = times + 1;
+        time = [[NSDate dateWithTimeIntervalSinceNow:(times)] retain];
+        [self removeChildByTag:100010 cleanup:true];
+        [self unschedule:@selector(pauseSchedule)];
+        [self schedule:@selector(updateTimeDisplay) interval:0.2];
+    }
 }
 //使用加时道具
 -(void)clickAddTimeTool{
     times = times + addTime;
     time = [[NSDate dateWithTimeIntervalSinceNow:(times)] retain];
+}
+//游戏暂停
+-(void)pauseGame{
+    [self onExit];
+}
+
+-(void)endPause{
+    [self onEnter];
 }
 @end

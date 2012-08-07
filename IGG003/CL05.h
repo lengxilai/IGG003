e05cc9fe439852b0ca379f2baca34252aac7263f
wangchong0114 <wangchong0114@vip.qq.com>
2012-08-07 18:05:17 +0800

    overgame

---
+++ IGG003/CL05.h
@@ -0,0 +1,13 @@
+//
+//  CL05.h
+//  IGG003
+//
+//  Created by wang chong on 12-8-7.
+//  Copyright (c) 2012年 ntt. All rights reserved.
+//
+
+#import "CCLayer.h"
+
+@interface CL05 : CCLayer
+
+@end

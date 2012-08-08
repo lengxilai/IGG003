3c2378823936a0a86b0ceed8fe4c880d3543eab0
wangchong0114 <wangchong0114@vip.qq.com>
2012-08-08 15:28:47 +0800

    11

--- IGG003/CL05.m
+++ IGG003/CL05.m
@@ -16,17 +16,40 @@
 -(id)init
 {
     if (self=[super init]) {
-        
-        [self enterGameOverGameLayer]; //所有动作停止；
         //加载游戏终了背景图片
         IGSprite *bak = [IGSprite spriteWithFile:@"paused.png"];
         bak.position = ccp(kWindowW/2,kWindowH/2);
         [self addChild:bak];
+        CCSprite* restartNormal=[CCSprite spriteWithSpriteFrameName:@"btn6-1.png"];
+        CCSprite* restartSecelt=[CCSprite spriteWithSpriteFrameName:@"btn6-2.png"];
+        restartSecelt.scale=0.95f;
         
-        [self writePlist];
+        CCMenuItemSprite* restartSprite=[CCMenuItemSprite itemFromNormalSprite:restartNormal selectedSprite:restartSecelt target:self selector:@selector(restartGame)];
+        
+        CCSprite* menuNormal=[CCSprite spriteWithSpriteFrameName:@"btn7-1.png"];
+        CCSprite* menuSecelt=[CCSprite spriteWithSpriteFrameName:@"btn7-2.png"];
+        restartSecelt.scale=0.95f;
+        
+        CCMenuItemSprite* menuSprite=[CCMenuItemSprite itemFromNormalSprite:menuNormal selectedSprite:menuSecelt target:self selector:@selector(gobackMenu)];
+        
+        CCMenu* menu=[CCMenu menuWithItems:restartSprite,menuSprite,nil]; //添加一个返回游戏按钮；
+        menu.position=ccp(kWindowW/2, 200);
+        [menu alignItemsVerticallyWithPadding:30];
+        [self addChild:menu];
+        //[self writePlist];
     }
     return self;
 }
+-(void)restartGame
+{
+    [[CCDirector sharedDirector] replaceScene:[S01 scene]];
+}
+
+-(void)gobackMenu
+{
+    [[CCDirector sharedDirector] replaceScene:[S00 scene]];
+}
+
 //读取plist
 -(void)readPlist{
     NSArray *doc = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);

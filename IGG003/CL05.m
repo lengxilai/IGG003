1e129324008ededc17d16084c50d817f3b066258
wangchong0114 <wangchong0114@vip.qq.com>
2012-08-11 14:20:35 +0800

    11

--- IGG003/CL05.m
+++ IGG003/CL05.m
@@ -17,31 +17,35 @@
 {
     if (self=[super init]) {
         
+        IGGameState *gameState = [IGGameState gameState];
+        //未破纪录
+        gameState.isBreakBest = NO;
         //加载游戏终了背景图片
         IGSprite *bak = [IGSprite spriteWithFile:@"paused.png"];
         bak.position = ccp(kWindowW/2,kWindowH/2);
         [self addChild:bak];
+        //restart按钮
         CCSprite* restartNormal=[CCSprite spriteWithSpriteFrameName:@"btn6-1.png"];
         CCSprite* restartSecelt=[CCSprite spriteWithSpriteFrameName:@"btn6-2.png"];
         restartSecelt.scale=0.95f;
         
         CCMenuItemSprite* restartSprite=[CCMenuItemSprite itemFromNormalSprite:restartNormal selectedSprite:restartSecelt target:self selector:@selector(restartGame)];
-        
+        //menu按钮
         CCSprite* menuNormal=[CCSprite spriteWithSpriteFrameName:@"btn7-1.png"];
         CCSprite* menuSecelt=[CCSprite spriteWithSpriteFrameName:@"btn7-2.png"];
         restartSecelt.scale=0.95f;
         
         CCMenuItemSprite* menuSprite=[CCMenuItemSprite itemFromNormalSprite:menuNormal selectedSprite:menuSecelt target:self selector:@selector(gobackMenu)];
-        
-        CCMenu* menu=[CCMenu menuWithItems:restartSprite,menuSprite,nil]; //添加一个返回游戏按钮；
+        //添加菜单
+        CCMenu* menu=[CCMenu menuWithItems:restartSprite,menuSprite,nil]; 
         menu.position=ccp(kWindowW/2, 100);
         [menu alignItemsHorizontallyWithPadding:30];
         [self addChild:menu];
-        
+        //取得得分
         int score = [self getGameScore];
         
         [self writePlistWithGameMode:[self getGameModeStr] withScore:score];
-        
+        //得分字体
         CCLabelBMFont *yourScoreStr = [CCLabelBMFont labelWithString:@"your score" fntFile:@"bitmapFont.fnt"];
         yourScoreStr.position = ccp(kWindowW/2,330);
         [self addChild:yourScoreStr];
@@ -50,6 +54,7 @@
         CCLabelBMFont *scoreStr = [CCLabelBMFont labelWithString:[NSString stringWithFormat:@"%d",score] fntFile:@"bitmapFont.fnt"];
         scoreStr.position = ccp(kWindowW/2,300);
         [self addChild:scoreStr];
+        //得分纪录
         CCLabelBMFont *bestScoreStr = [CCLabelBMFont labelWithString:@"best scores" fntFile:@"bitmapFont.fnt"];
         bestScoreStr.position = ccp(kWindowW/2,260);
         [self addChild:bestScoreStr];
@@ -100,7 +105,7 @@
 -(void)writePlistWithGameMode:(NSString *)gameMode withScore:(int)score{
     NSArray *scoreArr = [self readPlistWithGameMode:gameMode];
     NSMutableArray *newScoreArr = [[[NSMutableArray alloc] init] autorelease];
-    
+     IGGameState *gameState = [IGGameState gameState];
     if([scoreArr count] == 0){
         [newScoreArr addObject:[NSString stringWithFormat:@"%d",score]];
     }
@@ -111,6 +116,7 @@
             int bestScore =[[scoreArr objectAtIndex:i] intValue];
             
             if(bestScore <= score){
+                gameState.isBreakBest = YES;
                 //破纪录时
                 [newScoreArr addObject:[NSString stringWithFormat:@"%d",score]];
                 if([newScoreArr count] < 3){
@@ -125,7 +131,7 @@
             if([newScoreArr count] >= 3){
                 break;
             }
-        }else if([scoreArr count] != 0){
+        }else if([scoreArr count] != 0 && !gameState.isBreakBest){
             //纪录中少于三条的时候的最后一条
             [newScoreArr addObject:[NSString stringWithFormat:@"%d",score]];
             break;

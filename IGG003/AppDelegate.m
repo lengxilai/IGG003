7448d5a2bf7a203577606be9e8537ed2459fb31b
lipeng <lp508@163.com>
2012-08-21 15:55:05 +0800

    暂停bug修改

--- IGG003/AppDelegate.m
+++ IGG003/AppDelegate.m
@@ -131,9 +131,11 @@
 
 -(void) applicationDidEnterBackground:(UIApplication*)application {
 	[[CCDirector sharedDirector] stopAnimation];
-    //退出时显示暂停
-    S01 *s01 = [S01 getS01];
-    [s01 pauseGame];
+    //如果当前场景时游戏中时，退出时显示暂停
+    if([[[[[CCDirector sharedDirector] runningScene] children] lastObject] tag] == 10012){
+        S01 *s01 = [S01 getS01];
+        [s01 pauseGame]; 
+    }
 }
 
 -(void) applicationWillEnterForeground:(UIApplication*)application {
